64c340856998c8e94715862cddc8554ba7c8cda7
redcar1995 <benjamin1995202022@gmail.com>
2023-04-30 21:09:52 +0300

    advanced react hook first commit

---
+++ src/final/01.extra-1.js
@@ -0,0 +1,16 @@
+
+import * as React from 'react'
+
+const countReducer = (count, change) => count + change
+
+function Counter({initialCount = 0, step = 1}) {
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => changeCount(step)
+  return <button onClick={increment}>{count}</button>
+}
+
+function Usage() {
+  return <Counter />
+}
+
+export default Usage
